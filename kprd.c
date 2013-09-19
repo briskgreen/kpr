dc64db1d22a12ef2cca276a9c6c4e639d7daf576
briskgreen <briskgreen@163.com>
2013-09-19 20:39:45 +0800

    处理execl函数调用失败

--- kprd.c
+++ kprd.c
@@ -19,7 +19,14 @@ void init_daemon(const char *path,mode_t mask)
 void start_process(KPR *kpr)
 {
 	pid_t pid;
+	char status='\0';
+	int pipefd[2];
+	fd_set reads;
+	struct timeval timeout;
 
+	timeout.tv_sec=0;
+	timeout.tv_usec=1000;
+	pipe(pipefd);
 	while(kpr->next != NULL)
 	{
 		kpr=kpr->next;
@@ -27,13 +34,31 @@ void start_process(KPR *kpr)
 		if(pid == 0)
 		{
 			if(fork() == 0)
-				execl(kpr->path,kpr->name,kpr->arg,NULL);
+				if(execl(kpr->path,kpr->name,kpr->arg,NULL) == -1)
+				{
+					char status='\1';
+
+					write(pipefd[1],&status,sizeof(char));
+				}
 			wait(NULL);
 			_exit(0);
 		}
 		else
-			kpr->pid=pid;
+		{
+			FD_ZERO(&reads);
+			FD_SET(pipefd[0],&reads);
+			if(select(pipefd[0]+1,&reads,NULL,NULL,&timeout) > 0)
+				read(pipefd[0],&status,sizeof(char));
+
+			if(!status)
+				kpr->pid=pid;
+			else
+				kpr->pid=-1;
+		}
 	}
+
+	close(pipefd[1]);
+	close(pipefd[2]);
 }
 
 void kpr_run(KPR *kpr)
@@ -54,7 +79,14 @@ void kpr_run(KPR *kpr)
 void process_restart(KPR *kpr,pid_t pid)
 {
 	pid_t new_pid;
+	char status='\0';
+	fd_set reads;
+	int pipefd[2];
+	struct timeval timeout;
 
+	timeout.tv_sec=0;
+	timeout.tv_usec=1000;
+	pipe(pipefd);
 	while(kpr->next != NULL)
 	{
 		kpr=kpr->next;
@@ -64,10 +96,31 @@ void process_restart(KPR *kpr,pid_t pid)
 			new_pid=fork();
 
 			if(new_pid == 0)
-				execl(kpr->path,kpr->name,kpr->arg,NULL);
+			{
+				char status='\1';
+
+				if(execl(kpr->path,kpr->name,kpr->arg,NULL) == -1)
+					write(pipefd[1],&status,sizeof(char));
+			}
 			else
-				kpr->pid=new_pid;
+			{
+				FD_ZERO(&reads);
+				FD_SET(pipefd[0],&reads);
+				if(select(pipefd[0]+1,&reads,NULL,NULL,&timeout) > 0)
+					read(pipefd[0],&status,sizeof(char));
+
+				if(!status)
+					kpr->pid=new_pid;
+				else
+					kpr->pid=-1;
+			}
+
+			close(pipefd[1]);
+			close(pipefd[0]);
 			return;
 		}
 	}
+
+	close(pipefd[1]);
+	close(pipefd[0]);
 }
